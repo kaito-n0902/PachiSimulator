655de0b0150d95babc2b8be9c25ef1d28dc7aa65
kaito-n0902 <130465341+kaito-n0902@users.noreply.github.com>
2024-10-27 20:31:09 +0900

    Add files via upload

--- servlet/PachiTest.java
+++ servlet/PachiTest.java
@@ -1,12 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.FindMainKeyLogic;
+import model.FindPachiLogic;
+import model.InsertLogic;
 import model.Pachi;
 import model.PachiLogicEva;
 import model.PachiLogicRezero;
@@ -28,7 +33,9 @@ public class PachiTest extends HttpServlet {
 		int money = Integer.parseInt(request.getParameter("money"));
 		
 		//取得したリクエストパラメータでJavaBeansにてインスタンス化
-		Pachi pachi = new Pachi(model, money);
+		FindMainKeyLogic findbo = new FindMainKeyLogic();
+		int mainKey = findbo.findMainkey(model);
+		Pachi pachi = new Pachi(mainKey, model, money);
 		
 		//ロジックの呼び出し
 		if(model.equals("toloveru")) { //とらぶるを選んだ場合
@@ -44,6 +51,18 @@ public class PachiTest extends HttpServlet {
 		
 		//リクエストスコープにインスタンスを保存
 		request.setAttribute("pachi", pachi);
+		
+		//DBにレコードを追加
+		InsertLogic insertbo = new InsertLogic();
+		String modelName = pachi.getModel();
+		insertbo.execute(pachi, modelName);
+		
+		//DBから過去の結果を取得、セッションスコープに保存
+		FindPachiLogic findpachibo = new FindPachiLogic();
+		List<Pachi> pachiList = findpachibo.execute(modelName);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("pachiList", pachiList);
 		
 		//フォワード
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/pachiResult.jsp");
